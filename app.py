051ef2d1b341f823cf295f6a12649fa784f63d9b
Muneeb502 <mu0476080@gmail.com>
2024-04-08 23:53:04 +0500

    adding some more logics

--- app.py
+++ app.py
@@ -9,9 +9,15 @@ app = Flask(__name__)
 @app.route("/")
 def app_rout():
   return render_template("app.html")
-@app.route("/submitted" ,  methods=['POST'])
+@app.route("/submitted" ,  methods=['POST', 'GET'])
 def submitted():
-    return 'Form submitted successfully!'
+    name = request.form["name"]
+    email = request.form["email"]
+    message = request.form["messag"]
+    if (name and email and message) != ("" and " "):
+       return 'Form submitted successfully!'
+    else:
+       return 'kindly go back and fill the form !'
 
 if __name__ == ("__main__"):
     app.run(debug= True, port = 4001 )
